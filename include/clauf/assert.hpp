65a4bb3095f01293f54e3fade5a5593daf5d162d
Jonathan Müller <git@foonathan.net>
2022-08-25 11:08:56 +0200

    Add basic driver program

---
+++ include/clauf/assert.hpp
@@ -0,0 +1,43 @@
+// Copyright (C) 2022 Jonathan Müller and clauf contributors
+// SPDX-License-Identifier: BSL-1.0
+
+#ifndef CLAUF_ASSERT_HPP_INCLUDED
+#define CLAUF_ASSERT_HPP_INCLUDED
+
+#include <cassert>
+#include <cstdio>
+
+#define CLAUF_PRECONDITION(Condition) assert(Condition)
+#define CLAUF_ASSERT(Condition, Msg) assert((Condition) && (Msg))
+#define CLAUF_UNREACHABLE(Msg) assert(::clauf::_detail::unreachable(Msg))
+
+#define CLAUF_TODO(Msg) std::fprintf(stderr, "%s:%d: TODO: %s\n", __FILE__, __LINE__, (Msg))
+
+#define CLAUF_UNIMPLEMENTED(Msg)                                                                   \
+    (assert(::clauf::_detail::unimplemented(Msg)), ::clauf::_detail::unimplemented_value{})
+
+namespace clauf::_detail
+{
+constexpr bool unreachable(const char*)
+{
+    return false;
+}
+
+constexpr bool unimplemented(const char*)
+{
+    return false;
+}
+
+// Convertible to anything using UB.
+struct unimplemented_value
+{
+    template <typename T>
+    operator T&() const
+    {
+        return *static_cast<T*>(nullptr);
+    }
+};
+} // namespace clauf::_detail
+
+#endif // CLAUF_ASSERT_HPP_INCLUDED
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,66 @@
 // Copyright (C) 2022 Jonathan Müller and clauf contributors
 // SPDX-License-Identifier: BSL-1.0
 
+#include <CLI11.hpp>
 #include <cstdio>
+#include <lexy/input/file.hpp>
+#include <string>
 
-int main()
+#include <clauf/assert.hpp>
+
+namespace clauf
 {
-    std::puts("Hello World!");
+struct options
+{
+    std::string input;
+    bool        compile_only  = false;
+    bool        dump_ast      = false;
+    bool        dump_bytecode = false;
+};
+
+int main(const options& opts)
+{
+    auto file = lexy::read_file<lexy::utf8_encoding>(opts.input.c_str());
+    if (!file)
+    {
+        std::fprintf(stderr, "error: input file '%s' not found.\n", opts.input.c_str());
+        return 1;
+    }
+
+    CLAUF_TODO("compile file");
+    if (opts.dump_ast)
+    {
+        CLAUF_TODO("dump ast");
+    }
+
+    CLAUF_TODO("generate bytecode");
+    if (opts.dump_bytecode)
+    {
+        CLAUF_TODO("dump bytecode");
+    }
+
+    if (!opts.compile_only)
+    {
+        CLAUF_TODO("execute");
+    }
+
+    return 0;
+}
+} // namespace clauf
+
+int main(int argc, char* argv[])
+{
+    CLI::App       app("C interpreter");
+    clauf::options options;
+
+    app.add_option("input", options.input, "The C file to be interpreted.")->required();
+
+    app.add_flag("--compile-only", options.compile_only, "Only compile, don't execute.");
+    app.add_flag("--dump-ast", options.dump_ast, "Dump AST to stdout.");
+    app.add_flag("--dump-bytecode", options.dump_bytecode, "Dump Bytecode to stdout.");
+
+    CLI11_PARSE(app, argc, argv);
+
+    return clauf::main(options);
 }
 
